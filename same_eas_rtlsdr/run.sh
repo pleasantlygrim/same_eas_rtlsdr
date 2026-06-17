992e2f37b417f28133d65b76df7bf1b3b3fc948b
pleasantlygrim <pleasantlygrim@gmail.com>
2026-06-17 15:02:23 -0700

    Update run.sh

--- same_eas_rtlsdr/run.sh
+++ same_eas_rtlsdr/run.sh
@@ -14,11 +14,14 @@ echo "Audio stream: $AUDIO_STREAM"
 echo "Audio port: $AUDIO_PORT"
 
 if [ "$AUDIO_STREAM" = "true" ]; then
+  echo "Starting web UI on port 8080"
+  python3 /web.py &
+
   echo "Starting with audio stream enabled"
 
   rtl_fm -f "$FREQ" -M fm -s 22050 | tee \
     >(multimon-ng -a EAS -t raw - | python3 /same_eas.py "$EVENT_TYPE") \
-    >(sox -t raw -r 22050 -e signed-integer -b 16 -c 1 - -t mp3 - | nc -l -p "$AUDIO_PORT")
+    >(sox -t raw -r 22050 -e signed-integer -b 16 -c 1 - -t mp3 - > /tmp/noaa_stream.mp3)
 else
   echo "Starting without audio stream"
 
