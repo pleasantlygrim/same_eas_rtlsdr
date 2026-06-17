ed708c4e579eceaf6271bf47f8998c06d088c25a
pleasantlygrim <pleasantlygrim@gmail.com>
2026-06-17 14:58:07 -0700

    Update run.sh

--- same_eas_rtlsdr/run.sh
+++ same_eas_rtlsdr/run.sh
@@ -4,9 +4,23 @@ CONFIG_PATH=/data/options.json
 
 FREQ=$(jq -r '.frequency' "$CONFIG_PATH")
 EVENT_TYPE=$(jq -r '.event_type' "$CONFIG_PATH")
+AUDIO_STREAM=$(jq -r '.audio_stream' "$CONFIG_PATH")
+AUDIO_PORT=$(jq -r '.audio_port' "$CONFIG_PATH")
 
 echo "Starting SAME/EAS RTL-SDR"
 echo "Frequency: $FREQ"
 echo "HA event type: $EVENT_TYPE"
+echo "Audio stream: $AUDIO_STREAM"
+echo "Audio port: $AUDIO_PORT"
 
-rtl_fm -f "$FREQ" -M fm -s 22050 | multimon-ng -a EAS -t raw - | python3 /same_eas.py "$EVENT_TYPE"
+if [ "$AUDIO_STREAM" = "true" ]; then
+  echo "Starting with audio stream enabled"
+
+  rtl_fm -f "$FREQ" -M fm -s 22050 | tee \
+    >(multimon-ng -a EAS -t raw - | python3 /same_eas.py "$EVENT_TYPE") \
+    >(sox -t raw -r 22050 -e signed-integer -b 16 -c 1 - -t mp3 - | nc -l -p "$AUDIO_PORT")
+else
+  echo "Starting without audio stream"
+
+  rtl_fm -f "$FREQ" -M fm -s 22050 | multimon-ng -a EAS -t raw - | python3 /same_eas.py "$EVENT_TYPE"
+fi
